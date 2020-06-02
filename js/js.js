93d1f83ccbfa4c3cf4321aa1756b3fdf8285317a
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2020-06-02 23:20:10 +0300

    Сообщения

--- js/js.js
+++ js/js.js
@@ -11,7 +11,7 @@ let langObj = {
 
 let allSpans = document.getElementsByTagName('span');
 console.log(allSpans);
-for (let i = 0; i < allSpans.length; i++) {        
+for (let i = 0; i < allSpans.length; i++) {
     if (allSpans[i].textContent == 'html') {
         allSpans[i].classList.add(langObj.html);
     } else if (allSpans[i].textContent == 'css') {
@@ -20,11 +20,11 @@ for (let i = 0; i < allSpans.length; i++) {
         allSpans[i].classList.add(langObj.js);
     } else if (allSpans[i].textContent == 'jQuery') {
         allSpans[i].classList.add(langObj.jquery);
-    }  else if (allSpans[i].textContent == 'scss') {
+    } else if (allSpans[i].textContent == 'scss') {
         allSpans[i].classList.add(langObj.scss);
-    }  else if (allSpans[i].textContent == 'AJAX') {
+    } else if (allSpans[i].textContent == 'AJAX') {
         allSpans[i].classList.add(langObj.ajax);
-    }         
+    }
 }
 
 // Переадресация по клику на определённый проект
@@ -33,8 +33,8 @@ let myProjects = document.querySelectorAll('.myProjects__text .text__divs .myPro
     links = ["https://coralife-books.cf/", "https://kei-evenings.ml/", "https://ithub-coworking.com/", "https://coralife.cf/", "https://vasyl-pilka.ml/", "https://gregorys-delivery-food.tk"];
 
 
-for (let i = 0; i < myProjects.length; i++) {    
-    myProjects[i].addEventListener('click', function() {
+for (let i = 0; i < myProjects.length; i++) {
+    myProjects[i].addEventListener('click', function () {
         window.open(`${links[i]}`);
     });
 }
@@ -50,81 +50,140 @@ let emailInput = document.querySelector('input[type="email"]'),
     shower = document.getElementById('shower');
 
 function unfilledField(a) {
-   a.classList.add('unFilledField');
+    a.classList.add('unFilledField');
 }
+
 function filledField(b) {
-   b.classList.remove('unFilledField');
+    b.classList.remove('unFilledField');
 }
 
 function submitButtonLock() {
-   submitButton.setAttribute('disabled','disabled');
+    submitButton.setAttribute('disabled', 'disabled');
 }
 
 function submitButtonUnblock() {
-   submitButton.removeAttribute('disabled','disabled');
+    submitButton.removeAttribute('disabled', 'disabled');
 }
 
 function checkFields() {
-   if(emailInput.value.indexOf('@') == -1 || emailInput.value.length <= 10 || emailInput.value == '' || emailInput.value == ' ') {
-      unfilledField(fieldEmail);
-      submitButtonLock();
-      emailInput.placeholder = "Необходимо заполнить поле корректно!";
-   } else {
-      filledField(fieldEmail);
-      submitButtonUnblock();   
-      emailInput.placeholder = "";
-   } 
-   
-   if (nameInput.value.length <= 6) {
-       unfilledField(fieldName);
-      submitButtonLock();
-      nameInput.placeholder = "Необходимо заполнить поле корректно!";
-   } else {
-       filledField(fieldName);
-      submitButtonUnblock();
-      nameInput.placeholder = "";
-   }
+    if (emailInput.value.indexOf('@') == -1 || emailInput.value.length <= 10 || emailInput.value == '' || emailInput.value == ' ') {
+        unfilledField(fieldEmail);
+        submitButtonLock();
+        emailInput.placeholder = "Необходимо заполнить поле корректно!";
+    } else {
+        filledField(fieldEmail);
+        submitButtonUnblock();
+        emailInput.placeholder = "";
+    }
+
+    if (nameInput.value.length <= 6) {
+        unfilledField(fieldName);
+        submitButtonLock();
+        nameInput.placeholder = "Необходимо заполнить поле корректно!";
+    } else {
+        filledField(fieldName);
+        submitButtonUnblock();
+        nameInput.placeholder = "";
+    }
 }
 
 function hideshow() {
-	form.style.cssText="animation:hierrr 1s 1 linear;display:none;";
-	shower.style.cssText="display:block;animation:showwww 1s 1 linear;";
+    form.style.cssText = "animation:hierrr 1s 1 linear;display:none;";
+    shower.style.cssText = "display:block;animation:showwww 1s 1 linear;";
 }
 
 // Show modals
 let modalWindow = document.querySelector('.modal-window');
 
 function closeModal() {
-    modalWindow.style.opacity = 0 ;
+    modalWindow.style.opacity = 0;
     setTimeout(function () {
         modalWindow.style.display = "none";
-    },500);    
+    }, 500);
 }
 
-function showModal() {  
-    
+function showModal() {
+
     modalWindow.style.display = "flex";
     setTimeout(function () {
         modalWindow.style.opacity = 1;
-    },100); 
+    }, 100);
 }
 
-// Block F12
-document.onkeypress = function (event) {  
-    event = (event || window.event);  
-    if (event.keyCode == 123) {  
-       return false;  
-    }  
-    }  
-    document.onmousedown = function (event) {  
-    event = (event || window.event);  
-    if (event.keyCode == 123) {  
-       return false;  
-    }  
-    }  
-    document.onkeydown = function (event) {  
-    event = (event || window.event);  
-    if (event.keyCode == 123) {  
-       return false;  
-    }  
- };  
+// Блокировка F12 и ПКМ, создание сообщений
+
+
+let fisrtToast = `<div class="toast">
+                    <div class="toast-header">
+                        <strong class="mr-auto">Внимание!</strong>
+                        <button class="colse-toast">&times;</button>
+                    </div>
+                    <div class="toast-body">
+                        Во избежание плагиата, на сайте заблокированы такие сочетания клавиш как <code>F12</code>, и вызов контекстного меню (<code>ПКМ</code>)!
+                    </div>
+                  </div>`,
+    warningsContainer = document.querySelector('.warnings .chat'),
+    warningsMainCont = document.querySelector('.warnings');
+
+
+document.body.addEventListener('click', function (event) {
+    let target = event.target;
+    if (target.closest('.colse-toast')) {
+        target.closest('.toast').style.cssText = "animation: toastGoodBye .3s linear 1 both;";
+        setTimeout(function () {
+            target.closest('.toast').remove();
+        }, 500);
+    }
+
+    if (target.closest('.info-panel button')) {
+        target.style.right = "-350px";
+        target.closest('.warnings').style.right = "-350px";
+    }
+
+});
+
+let num = 0;
+
+function showToast() {
+    let toast = document.querySelectorAll('.toast');
+    warningsMainCont.style.right = "0px";
+    if (!toast[0]) {
+        warningsContainer.insertAdjacentHTML('afterbegin', fisrtToast);
+    } else {
+        warningsContainer.insertAdjacentHTML('afterbegin', `
+        <div class="toast">
+            <div class="toast-header">
+                <strong class="mr-auto">Внимание!</strong>
+                <button class="colse-toast">&times;</button>
+            </div>
+            <div class="toast-body">
+                Во избежание плагиата, на сайте заблокированы такие сочетания клавиш как <code>F12</code>, и вызов контекстного меню (<code>ПКМ</code>)!
+            </div>
+        </div>`);
+        num++;
+    }
+}
+
+document.onkeypress = function (event) {
+    event = (event || window.event);
+    if (event.keyCode == 123) {
+        showToast();
+        return false;
+    }
+};
+
+document.onmousedown = function (event) {
+    event = (event || window.event);
+    if (event.keyCode == 123) {
+        showToast();
+        return false;
+    }
+};
+
+document.onkeydown = function (event) {
+    event = (event || window.event);
+    if (event.keyCode == 123) {
+        showToast();
+        return false;
+    }
+};
